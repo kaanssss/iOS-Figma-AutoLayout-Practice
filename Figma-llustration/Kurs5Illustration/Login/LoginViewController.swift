599be78c2e368117b6e92558def956a79c09f759
Kaan Yalçınkaya <74143983+kaanssss@users.noreply.github.com>
2022-03-29 17:00:43 +0300

    Add files via upload

---
+++ Figma-llustration/Kurs5Illustration/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kurs5Illustration
+//
+//  Created by Kaan Yalçınkaya on 21.11.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
